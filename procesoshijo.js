76dd3c3118eabfd606475091a91b49a357b94984
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-05-29 23:17:38 +0200

    Notas sobre los procesos

--- procesoshijo.js
+++ procesoshijo.js
@@ -1,3 +1,11 @@
+/*
+    Proceso es la ejecución de algo(programa, commando, etc) que terminara en un tiempo finito.
+
+    Cuando se ejecuta un programa estos corren sobre la terminal y cuando necesitan crear un nuevo proceso crean una nueva terminal(Parent shell).
+
+    Node funciona a nivel SO creando child shell en los que es posible ejecutar cualquier instrucción.
+*/
+
 var { exec, spawn } = require("child_process");
 
 exec("node controlerror.js", (err, stdout, sterr) => {
